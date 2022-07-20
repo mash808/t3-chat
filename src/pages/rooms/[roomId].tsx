bd8ce617504a34140b39719f8c03abc98dfe33cf
Asher <n10255087@qut.edu.au>
2022-07-20 21:32:28 +1000

    final styling

--- src/pages/rooms/[roomId].tsx
+++ src/pages/rooms/[roomId].tsx
@@ -6,7 +6,15 @@ import { Message } from "../../constants/schemas"
 import { trpc } from "../../utils/trpc"
 
 function MessageItem({message, session}: {message: Message, session: Session}) {
-   return <li>
+   const baseStyles = 'mb-4 text-md w-7/12 p-4 text-gray-700 border border-gray-700 rounded-md'
+   const liStyles = message.sender.name === session.user?.name ? baseStyles : baseStyles.concat(' self-end bg-gray-700 text-white') 
+
+   return <li className={liStyles}>
+      <div className="flex">
+         <time>{message.sentAt.toLocaleTimeString('en-AU', {
+            timeStyle: 'short'
+         })} - {message.sender.name}</time>
+      </div>
       {message.message}
    </li>
 }
@@ -35,21 +43,25 @@ function RoomPage() {
       return <div><button onClick={() => signIn()}>Login</button></div>
    }
 
-   return <div>
-         <ul>
+   return <div className="flex flex-col h-screen">
+      <div className="flex-1">
+         <ul className="flex flex-col p-4">
             {messages.map((m) => {
                return <MessageItem key={m.id} message={m} session={session}/>
             })}
          </ul>
-         <form onSubmit={(e) => {
+      </div>
+         <form className='flex' onSubmit={(e) => {
             e.preventDefault()
             sendMessageMutation({
                roomId, 
                message
             })
+
+            setMessage('')
          }}>
-            <textarea value={message} onChange={(e) => setMessage(e.target.value)} placeholder="What do you want to say?"/>
-            <button type='submit'>
+            <textarea className="black p-2.5 w-full text-gray-700 bg-gray-50 rounded-md border border-gray-700" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="What do you want to say?"/>
+            <button className='flex-1 text-white bg-gray-900 p-2.5' type='submit'>
                Send message
             </button>
          </form>
