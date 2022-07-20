43cb5e6205c956175c62cce1b5c9e84b07e06a30
Asher <n10255087@qut.edu.au>
2022-07-20 20:06:50 +1000

    server logic

--- src/server/router/room.ts
+++ src/server/router/room.ts
@@ -1,3 +1,42 @@
+import * as trpc from '@trpc/server';
+import { randomUUID } from "crypto";
+import { Events } from "../../constants/events";
+import { Message, messageSubSchema, sendMessageSchema } from "../../constants/schemas";
 import { createRouter } from "./context";
 
 export const roomRouter = createRouter()
+   .mutation('send-message', {
+      input: sendMessageSchema,
+      resolve({ctx, input}) {
+         const message: Message = {
+            id: randomUUID(),
+            ...input,
+            sentAt: new Date(),
+            sender: {
+               name: ctx.session?.user?.name || "unknown"
+            }
+         }
+
+         ctx.ee.emit(Events.SEND_MESSAGE, message)
+
+         return true
+      }
+   })
+   .subscription('onSendMessage', {
+      input: messageSubSchema,
+      resolve({ctx,  input}) {
+         return new trpc.Subscription<Message>((emit) => {
+            function onMessage(data: Message) {
+               if (input.roomId === data.roomId) {
+                  emit.data(data)
+               }
+            }
+
+            ctx.ee.on(Events.SEND_MESSAGE, onMessage)
+
+            return () => {
+               ctx.ee.off(Events.SEND_MESSAGE, onMessage)
+            }
+         });
+      }
+   })
